ac4db9af83ce4d5982a36c5201d652f69a82c6cf
moses12-bi <mosesebagabo01@gmail.com>
2025-06-24 00:28:16 +0200

    Update source files and sitemap

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/portfolio1/',
   plugins: [react()],
   css: {
     postcss: {
